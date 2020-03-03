5eba0820df5f95a0ca7b4c7327d6ceff7e06beed
Henrique <riquetufano@gmail.com>
2020-03-02 21:50:41 -0400

    primeiro

---
+++ hen.py
@@ -0,0 +1,23 @@
+def media2(n1,n2):
+ x=(n1+n2)/2
+ print("Aqui sua media merda: {}".format(x))
+ return x
+def media():
+    n1 = int(input("Digite um numero:"))
+    n2 = int(input("Digite um numero:"))
+    x=(n1+n2)/2
+    print("Aqui sua media merda: {}".format(x))
+    return x
+
+a=int(input("digite um numero: "))
+b=int(input("digite um numero: "))
+c=media2(a,b)
+#media()
+#media()
+#media()
+
+#a=media()
+#b=media()
+#c=media()
+#print(a,b,c)
+
